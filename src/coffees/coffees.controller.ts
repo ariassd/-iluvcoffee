31179fa3a6856e3dd3113cab2cf15497d7f05336
Luis Arias <ariassd@gmail.com>
2020-08-21 16:22:37 -0600

    Usin repository

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,34 +11,38 @@ import {
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesSv: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
+  findAll(/*@Query() paginationQuery: any*/): Promise<Coffee[]> {
+    // const { limit, offset } = paginationQuery;
     return this.coffeesSv.findAll();
   }
 
   @Get(':id')
-  findById(@Param('id') id: string) {
+  findById(@Param('id') id: string): Promise<Coffee> {
     return this.coffeesSv.findOne(id);
   }
 
   @Post()
-  create(@Body() createCoffeeDto: CreateCoffeeDto): void {
+  create(@Body() createCoffeeDto: CreateCoffeeDto): Promise<Coffee> {
     return this.coffeesSv.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCoffeeDto: UpdateCoffeeDto,
+  ): Promise<Coffee> {
     return this.coffeesSv.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<Coffee> {
     return this.coffeesSv.remove(id);
   }
 }
